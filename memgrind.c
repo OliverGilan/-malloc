7a0a521c9e0b3f5518ae32b35f052d3b28cae507
Oliver Gilan <olivergilan@gmail.com>
2019-10-05 17:10:54 -0400

    Wrote tests A and B in memgrind

--- memgrind.c
+++ memgrind.c
@@ -1,6 +1,39 @@
 #include "mymalloc.h"
 #include <time.h>
 
+void testA();
+void testB();
+void testC();
+
 int main(){
+
+}
+
+void testA(){
+    int i = 0;
+    while(i<150){
+        char* pointer = (char*)malloc(1);
+        free(pointer);
+    }
+}
+
+void testB(){
+    char* pointers = (char*)malloc(50*sizeof(char));
+    int i = 0;
+    int j = 0;
+    while(j<3){
+        while(i < 50){
+            pointers[i] = (char*)malloc(1);
+            i++;
+        }
+        while(i>=0){
+            free(pointers[i-1]);
+            i--;
+        }
+        j++;
+    }
+}
+
+void testC(){
     
 }
