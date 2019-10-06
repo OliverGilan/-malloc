0846ec79313ec7fe5d04884ca55fe8dd2decdf15
Oliver Gilan <olivergilan@gmail.com>
2019-10-05 21:22:13 -0400

    Finished test cases C and D

--- memgrind.c
+++ memgrind.c
@@ -1,11 +1,15 @@
 #include "mymalloc.h"
 #include <time.h>
+#include <stdlib.h>
 
 void testA();
 void testB();
 void testC();
+void testD();
 
 int main(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
 
 }
 
@@ -35,5 +39,56 @@ void testB(){
 }
 
 void testC(){
-    
+    time_t t;
+    srand((unsigned) time(&t));
+
+    int m = 0;
+    char* pointers = NULL;
+    int i = 0;
+    while(m<50){
+        int k = rand();
+        if(k%2==0){
+            m++;
+            pointers[i] = (char*)malloc(1);
+            i++; 
+        }else{
+            if(i>0){
+                free(pointers[i-1]);
+                i--;
+            }
+        }
+    }
+    while(i>0){
+        free(pointers[i-1]);
+    }
+}
+
+void testD(){
+    time_t t;
+    srand((unsigned) time(&t));
+
+    int m = 0;
+    int t = 0;
+    char** pointers = (char*)malloc(50*sizeof(char*));
+    int i = 0;
+    while(m<50){
+        int k = rand();
+        if(k%2==0){
+            m++;
+            t=t+(k%64)+1;
+            if(t>4096){
+                //DO SOMETHING
+            }
+            pointers[i] = (char*)malloc((k%64)+1);
+            i++; 
+        }else{
+            if(i>0){
+                free(pointers[i-1]);
+                i--;
+            }
+        }
+    }
+    while(i>0){
+        free(pointers[i-1]);
+    }
 }
