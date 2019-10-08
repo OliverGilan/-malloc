71096a06b69fae07e4bbc60309eaf64f0e248c2b
Rishab <rishabravikumar@yahoo.com>
2019-10-08 18:02:45 -0400

    Update malloc.c

---
+++ mymalloc.c
@@ -0,0 +1,50 @@
+#include "mymalloc.h"
+
+void* mymalloc(int size, int line, char* file) {
+	int start = 1;
+	void* ptr = &myblock[0];
+
+	for (int i = 0; i < sizeof(myblock) - size + 1; i++) {
+		if (tsize == 0) {
+			myblock[i] = 'A';
+			break;
+		}
+		if (myblock[i] != 'A') {
+			if (tsize == size) {
+				ptr = &myblock[i];
+			}
+			tsize--;
+			continue;
+		}
+		if (myblock[i] == 'A') {
+			tsize = size;
+			continue;
+		}
+		if (myblock[i] == 'B') {
+			int usable = 1;
+			for (int j = i; j < sizeof(myblock); j++) {
+				if (myblock[i] == 'A') {
+					usable = 0;
+					break;
+				}
+				if (myblock[i] == 'B')
+					break;
+			}
+			if (usable == 0) {
+				tsize = size;
+				continue;
+			}
+			if (usable == 1) {
+				myblock[i] = "\0";
+				tsize--;
+				continue;
+			}
+		}
+
+	}
+	return ptr;
+}
+
+void myfree(char* ptr, int line, char* file) {
+
+}
